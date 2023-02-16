ed128bb53bf8e4c403cef1526338e581c3bb2d67
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-16 17:05:59 +0000

    Notificacion

--- web/notificacion/ModeloMensaje.php
+++ web/notificacion/ModeloMensaje.php
@@ -1,13 +1,18 @@
 <?php
+	$DB_HOST = $_ENV['DB_HOST'];
+	$DB_USER = $_ENV['DB_USER'];
+	$DB_PASSWORD = $_ENV['DB_PASSWORD'];
+	$DB_NAME = $_ENV['DB_NAME'];
+	$DB_PORT = $_ENV['DB_PORT'];
 
 class ModeloMensaje  {
 
   public function conectar_bd()
   {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "app_msg";
+    $servername = $DB_HOST;
+    $username = $DB_USER;
+    $password = $DB_PASSWORD;
+    $dbname = $DB_NAME;
     /* crear la conexión */
     $conn = new mysqli($servername, $username, $password, $dbname);
     /* comprobar la conexión */
