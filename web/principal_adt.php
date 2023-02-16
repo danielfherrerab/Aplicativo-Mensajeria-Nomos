96e4024eecbf0d1d0cc7532040e9032bebb9d144
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-16 19:37:07 +0000

    sera?

--- web/principal_adt.php
+++ web/principal_adt.php
@@ -21,15 +21,15 @@
   $solofecha_hoy      = date("Y-m-d");
   $fecha_despues 	= date("Y-m-d 23:59:59",strtotime($fecha_hoy."+ 1 day,"));
 
-  if(isset($_GET['encarg'])) {
-    $num_enc = $_GET['encarg'];
-    $cam__estado = $_GET['est'];
-    $rectificar = mysqli_query($conexion,"UPDATE encargos set estado = '$cam__estado',fecha_completado = '$fecha_hoy' where id_encargo = $num_enc");
+  // if(isset($_GET['encarg'])) {
+  //   $num_enc = $_GET['encarg'];
+  //   $cam__estado = $_GET['est'];
+  //   $rectificar = mysqli_query($conexion,"UPDATE encargos set estado = '$cam__estado',fecha_completado = '$fecha_hoy' where id_encargo = $num_enc");
 
-    if($rectificar){
-      echo "<script> alert('Se cambio de estado'); window.location.href ='principal_adt.php'; </script>";
-    }
-  }
+  //   if($rectificar){
+  //     echo "<script> alert('Se cambio de estado'); window.location.href ='principal_adt.php'; </script>";
+  //   }
+  // }
   if(isset($_GET['registrado'])) {
     echo "<div class='registrado'>Se ha registrado correctamente </div>";
   }
