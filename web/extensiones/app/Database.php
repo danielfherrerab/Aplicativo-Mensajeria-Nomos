05110ff5d8268752e0467ae1e3120522e464268d
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-16 17:03:58 +0000

    Database

--- web/extensiones/app/Database.php
+++ web/extensiones/app/Database.php
@@ -1,40 +1,46 @@
 <?php
 
-class Database {
+	$DB_HOST = $_ENV['DB_HOST'];
+	$DB_USER = $_ENV['DB_USER'];
+	$DB_PASSWORD = $_ENV['DB_PASSWORD'];
+	$DB_NAME = $_ENV['DB_NAME'];
+	$DB_PORT = $_ENV['DB_PORT'];
 
-	private $db;
+	class Database {
 
-	public function __construct () {
-		$this->db = new mysqli("localhost", "root", "", "app_msg");
-	}
+		private $db;
 
-	public function query($sql) {
-		return $this->db->query($sql);
-	}	
-
-	public function fetchData($result) {
-		if (!$result) return [];
-		if ($result->num_rows === 0) return [];
-
-		if ($result->num_rows === 1) {
-			return [$result->fetch_array()];
+		public function __construct () {
+			$this->db = new mysqli($DB_HOST, $$DB_USER, $DB_PASSWORD, $DB_NAME);
 		}
 
-		$data = [];
+		public function query($sql) {
+			return $this->db->query($sql);
+		}	
 
-		while ($row = $result->fetch_array()) {
-			array_push($data, $row);
+		public function fetchData($result) {
+			if (!$result) return [];
+			if ($result->num_rows === 0) return [];
+
+			if ($result->num_rows === 1) {
+				return [$result->fetch_array()];
+			}
+
+			$data = [];
+
+			while ($row = $result->fetch_array()) {
+				array_push($data, $row);
+			}
+
+			return $data;
 		}
 
-		return $data;
-	}
+		public function getConnectionInstance() {
+			return $this->db;
+		}
 
-	public function getConnectionInstance() {
-		return $this->db;
-	}
+		public function closeConnection() {
+			$this->db->close();
+		}
 
-	public function closeConnection() {
-		$this->db->close();
 	}
-
-}
