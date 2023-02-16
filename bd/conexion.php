e4b2ad1b028248b7277f1c7b92943ef286dabdbf
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-16 12:44:47 +0000

    Conexion

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,8 +1,14 @@
 <?php
+	$BD_HOST = $_ENV['BD_HOST'];
+	$BD_NAME = $_ENV['BD_NAME'];
+	$BD_PASSWORD = $_ENV['BD_PASSWORD'];
+	$BD_PORT = $_ENV['BD_PORT'];
+	$BD_USER = $_ENV['BD_USER'];
+
 	class Conexion extends mysqli {
 
 		public function __construct() {
-			parent::__construct('localhost','root','','app_msg');
+			parent::__construct($BD_HOST,$BD_USER,$BD_PASSWORD,$BD_NAME);
 			$this->query("SET NAMES 'utf8';");
 			$this->connect_errno ? die ('Error con la conexion') : $exito = 'Conectado';
 			//echo $exito;
@@ -11,5 +17,5 @@
 		}
 
 	}
-  $conexion=mysqli_connect('localhost','root','','app_msg') or die ('problemas en la conexion');
+  $conexion=mysqli_connect($BD_HOST,$BD_USER,$BD_PASSWORD,$BD_NAME) or die ('problemas en la conexion');
 ?>
