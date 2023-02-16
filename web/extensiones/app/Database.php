01988ebc56f7c797e9eccd3b33d4822bb28bf2b5
danielfherrerab <102389796+danielfherrerab@users.noreply.github.com>
2023-02-16 17:07:57 +0000

    Conexion

--- web/extensiones/app/Database.php
+++ web/extensiones/app/Database.php
@@ -1,17 +1,17 @@
 <?php
 
-	$DB_HOST = $_ENV['DB_HOST'];
-	$DB_USER = $_ENV['DB_USER'];
-	$DB_PASSWORD = $_ENV['DB_PASSWORD'];
-	$DB_NAME = $_ENV['DB_NAME'];
-	$DB_PORT = $_ENV['DB_PORT'];
-
 	class Database {
+
+		public $DB_HOST;
+		public $DB_USER;
+		public $DB_PASSWORD;
+		public $DB_NAME;
+		public $DB_PORT;
 
 		private $db;
 
 		public function __construct () {
-			$this->db = new mysqli($DB_HOST, $$DB_USER, $DB_PASSWORD, $DB_NAME);
+			$this->db = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 		}
 
 		public function query($sql) {
